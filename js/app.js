d21c542969c3688ab6f8467db94c9407da624e80
Felipe-Berraz <fberraz@udesa.edu.ar>
2021-11-23 09:54:32 -0300

    update

--- js/app.js
+++ js/app.js
@@ -13,87 +13,75 @@ import {
 let actualLocation = location.pathname
 
 // get Data from each page
-switch (actualLocation) {
-    case '/index.html':
-        // get Top Rated Movies for Banner
-        let bannerContainer = document.getElementById('banner-container')
-        getMoviesSeries(bannerContainer, 'movie/popular', 'banner')
+if(actualLocation == '/index.html'){
+    // get Top Rated Movies for Banner
+    let bannerContainer = document.getElementById('banner-container')
+    getMoviesSeries(bannerContainer, 'movie/popular', 'banner')
 
-        // get Top Rated Movies
-        let topRatedContainerMovies = document.getElementById('top-rated-movies')
-        getMoviesSeries(topRatedContainerMovies, 'movie/top_rated', 'movie')
+    // get Top Rated Movies
+    let topRatedContainerMovies = document.getElementById('top-rated-movies')
+    getMoviesSeries(topRatedContainerMovies, 'movie/top_rated', 'movie')
 
-        // get Top Rated Series
-        let topRatedContainerTv = document.getElementById('top-rated-series')
-        getMoviesSeries(topRatedContainerTv, 'tv/top_rated', 'movie')
+    // get Top Rated Series
+    let topRatedContainerTv = document.getElementById('top-rated-series')
+    getMoviesSeries(topRatedContainerTv, 'tv/top_rated', 'movie')
 
-        // get Popular Movies
-        let popularMoviesContainer = document.getElementById('popular-movies')
-        getMoviesSeries(popularMoviesContainer, 'movie/popular', 'movie')
+    // get Popular Movies
+    let popularMoviesContainer = document.getElementById('popular-movies')
+    getMoviesSeries(popularMoviesContainer, 'movie/popular', 'movie')
 
-        // get Popular Series
-        let popularSeriesContainer = document.getElementById('popular-series')
-        getMoviesSeries(popularSeriesContainer, 'tv/popular', 'tv')
+    // get Popular Series
+    let popularSeriesContainer = document.getElementById('popular-series')
+    getMoviesSeries(popularSeriesContainer, 'tv/popular', 'tv')
 
-        // get Trending Today
-        let trendingToday = document.getElementById('trending-today')
-        getTrendingDay(trendingToday)
-        break;
-    case '/series.html':
-        // get Top Rated Series
-        let topRatedContainerTvHTML = document.getElementById('top-rated-series')
-        getMoviesSeries(topRatedContainerTvHTML, 'tv/top_rated', 'movie')
+    // get Trending Today
+    let trendingToday = document.getElementById('trending-today')
+    getTrendingDay(trendingToday)
+} else if(actualLocation == '/series.html'){
+    // get Top Rated Series
+    let topRatedContainerTvHTML = document.getElementById('top-rated-series')
+    getMoviesSeries(topRatedContainerTvHTML, 'tv/top_rated', 'movie')
 
-        // get Popular Series
-        let popularSeriesContainerHTML = document.getElementById('popular-series')
-        getMoviesSeries(popularSeriesContainerHTML, 'tv/popular', 'tv')
+    // get Popular Series
+    let popularSeriesContainerHTML = document.getElementById('popular-series')
+    getMoviesSeries(popularSeriesContainerHTML, 'tv/popular', 'tv')
+}else if(actualLocation == '/movies.html'){
+    // get Top Rated Movies
+    let topRatedContainerMoviesHTML = document.getElementById('top-rated-movies')
+    getMoviesSeries(topRatedContainerMoviesHTML, 'movie/top_rated', 'movie')
 
-        break;
-    case '/movies.html':
-        // get Top Rated Movies
-        let topRatedContainerMoviesHTML = document.getElementById('top-rated-movies')
-        getMoviesSeries(topRatedContainerMoviesHTML, 'movie/top_rated', 'movie')
+    // get Popular Movies
+    let popularMoviesContainerHTML = document.getElementById('popular-movies')
+    getMoviesSeries(popularMoviesContainerHTML, 'movie/popular', 'movie')
+}else if(actualLocation == '/details.html'){
+    //get Movie or Serie
+    let detailsContainer = document.getElementById('detail-container')
+    let mainCastWrapper = document.getElementById('main-cast-wrapper')
+    let asideInfo = document.getElementById('more-info')
+    let collectionContainer = document.getElementById('collection')
+    let actualSeason = document.getElementById('actual-season')
+    getOneMovieTv(detailsContainer, mainCastWrapper, asideInfo, collectionContainer, actualSeason)
 
-        // get Popular Movies
-        let popularMoviesContainerHTML = document.getElementById('popular-movies')
-        getMoviesSeries(popularMoviesContainerHTML, 'movie/popular', 'movie')
-        break;
-    case '/details.html':
-        //get Movie or Serie
-        let detailsContainer = document.getElementById('detail-container')
-        let mainCastWrapper = document.getElementById('main-cast-wrapper')
-        let asideInfo = document.getElementById('more-info')
-        let collectionContainer = document.getElementById('collection')
-        let actualSeason = document.getElementById('actual-season')
-        getOneMovieTv(detailsContainer, mainCastWrapper, asideInfo, collectionContainer, actualSeason)
+    // get Similar Titles
+    let similarTitles = document.getElementById('similar-titles')
+    getSimilarTitles(similarTitles)
+}else if(actualLocation == '/results.html'){
+    //get Results from search
+    let searchResultsContainer = document.getElementById('results-container')
+    getResultsSearch(searchResultsContainer)
+}else if(actualLocation == '/castCrew.html'){
+    let castCrewContainer = document.getElementById('cast-crew-container')
+    getCastCrew(castCrewContainer)
+}else if(actualLocation == '/genres.html'){
+    // get Genres Movies
+    let genresMoviesContainer = document.getElementById('genres-movies')
+    getGenres(genresMoviesContainer, 'movie')
 
-        // get Similar Titles
-        let similarTitles = document.getElementById('similar-titles')
-        getSimilarTitles(similarTitles)
-        break;
-    case '/results.html':
-        //get Results from search
-        let searchResultsContainer = document.getElementById('results-container')
-        getResultsSearch(searchResultsContainer)
-        break;
-    case '/castCrew.html': //santi
-        let castCrewContainer = document.getElementById('cast-crew-container')
-        getCastCrew(castCrewContainer)
-        break;
-    case '/genres.html':
-        // get Genres Movies
-        let genresMoviesContainer = document.getElementById('genres-movies')
-        getGenres(genresMoviesContainer, 'movie')
-
-        // get Genres Series
-        let genresSeriesContainer = document.getElementById('genres-series')
-        getGenres(genresSeriesContainer, 'tv')
-        break;
-    case '/titleSameGenre.html':
-        //get Titles with the same genre
-        let titleSameGenreContainer = document.getElementById('title-same-genre-container')
-        getTitleSameGenre(titleSameGenreContainer)
-        break;
-    default:
-        break;
+    // get Genres Series
+    let genresSeriesContainer = document.getElementById('genres-series')
+    getGenres(genresSeriesContainer, 'tv')
+}else if(actualLocation == '/titleSameGenre.html'){
+    //get Titles with the same genre
+    let titleSameGenreContainer = document.getElementById('title-same-genre-container')
+    getTitleSameGenre(titleSameGenreContainer)
 }
